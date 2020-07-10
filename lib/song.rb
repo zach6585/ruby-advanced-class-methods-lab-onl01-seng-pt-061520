ba8345514d614af79ee4b38fcfa804d7ab82da04
zachary mines <zmines@wesleyan.edu>
2020-07-10 20:18:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,41 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create 
+    song = self.new 
+    @@all << song
+    return song 
+  end 
 
   def save
     self.class.all << self
   end
-
+  
+  def self.new_by_name(song)
+    songs = self.new 
+    songs.name = song
+    songs 
+  end 
+  
+  def self.create_by_name(song)
+    songs = self.new 
+    songs.name = song
+    @@all << songs
+    songs 
+  end 
+  
+  def self.find_by_name(song)
+    self.all.find{|seng| seng.name == song}
+  end 
+  
+  def self.find_or_create_by_name(song)
+    a = self.all.find{|seng| seng.name == song} 
+    songs = self.new 
+    if a == nil 
+      self.create_by_name(song)
+    else 
+      a 
+    end 
+  end 
 end
